17a68c3e3f95458e3fdb40a8997850b948c0e599
edine-noella <edinenoella@gmail.com>
2024-04-30 09:24:03 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.html'],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#ef4444',
+        'secondary': '#353535',
+      },
+    },
+  },
+  plugins: [],
+}
+
